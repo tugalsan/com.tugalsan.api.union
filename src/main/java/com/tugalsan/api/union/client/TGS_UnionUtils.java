f66bc210f569aa2c6f69b696a1c4243e4d546b2d
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-04 08:25:25 +0300

    Create TGS_UnionUtils.java

---
+++ src/main/java/com/tugalsan/api/union/client/TGS_UnionUtils.java
@@ -0,0 +1,27 @@
+package com.tugalsan.api.union.client;
+
+public class TGS_UnionUtils {
+
+    public static RuntimeException toRuntimeException(CharSequence className, CharSequence funcName, Object errorContent) {
+        return new RuntimeException(TGS_UnionUtils.class + ".toRuntimeException->CLASS[" + className + "] -> FUNC[" + funcName + "] -> ERR: " + errorContent);
+    }
+
+    public static void thrw(CharSequence className, CharSequence funcName, Object errorContent) {
+        throw toRuntimeException(className, funcName, errorContent);
+    }
+
+    public static <R> R thrwReturns(CharSequence className, CharSequence funcName, Object errorContent) {
+        throw toRuntimeException(className, funcName, errorContent);
+    }
+
+    public static void runNothing() {
+    }
+
+    public static <R> R callNull() {
+        return null;
+    }
+
+    public static <R> R callValue(R result) {
+        return result;
+    }
+}
