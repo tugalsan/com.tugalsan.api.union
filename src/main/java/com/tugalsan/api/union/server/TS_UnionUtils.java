17a29b279a8304993e731e092bb6ae3428eff639
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-05 05:36:35 +0300

    Update TS_UnionUtils.java

--- src/main/java/com/tugalsan/api/union/server/TS_UnionUtils.java
+++ src/main/java/com/tugalsan/api/union/server/TS_UnionUtils.java
@@ -2,7 +2,7 @@ package com.tugalsan.api.union.server;
 
 public class TS_UnionUtils {
 
-    public static <R> R throwIfRuntimeException(Throwable t) {
+    public static <R> R throwAsRuntimeExceptionIfInterruptedException(Throwable t) {
         if (t instanceof InterruptedException) {
             Thread.currentThread().interrupt();
         }
